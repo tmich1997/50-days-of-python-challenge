3e23052c6dbc8e7df3158d4a277b84a01ed8756b
tmich1997 <nu842639@gmail.com>
2023-12-05 14:21:22 +1100

    completed day 8

---
+++ day_08_odd_and_even.py
@@ -0,0 +1,35 @@
+# Day 07, A String Range
+
+def odd_even(lst_numbers):
+    max_num = lst_numbers[0]
+    min_num = lst_numbers[0]
+
+    for i in lst_numbers:
+        if i%2 == 0 and i > max_num:
+            max_num = i
+        elif i%2 != 0 and i < min_num:
+            min_num = i
+    return max_num - min_num
+
+numbers = [1, 2, 4, 6]
+
+func_call = odd_even(numbers)
+print(func_call)
+
+# Extra Challenge: List of Prime Numbers
+
+input_number = int(input("Please enter a number greater than 2: "))
+
+def prime_numbers(inp_number):
+    if inp_number < 2:
+        return False
+
+    for i in range(2, int(inp_number**0.5) +1):
+        if inp_number % i == 0:
+            return False
+    
+    return True
+
+for num in range(2, input_number + 1):
+    if prime_numbers(num):
+        print(num)
